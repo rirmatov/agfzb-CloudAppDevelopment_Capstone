9ac3207188c8859c2dee745a17295666ba237ec7
rirmatov <50174905+rirmatov@users.noreply.github.com>
2022-11-26 18:40:36 +0600

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost",'djangoserver-space-iravshan.us-south.cf.appdomain.cloud']
 
 
 # Application definition
