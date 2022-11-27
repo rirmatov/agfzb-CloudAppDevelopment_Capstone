63fb433cf57f6cdf191e267a8dc8d3e510ff1025
rirmatov <50174905+rirmatov@users.noreply.github.com>
2022-11-27 18:41:09 +0600

    Create review.py

---
+++ functions/sample/python/review.py
@@ -0,0 +1,37 @@
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
+import sys
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core import ApiException
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+def main(dict): 
+    authenticator = IAMAuthenticator("API_KEY")
+    service = CloudantV1(authenticator = authenticator)
+        
+    service.set_service_url("COUCH_URL")
+        
+    response = service.post_document(
+        db= "reviews",
+        document = (dict["review"])
+        ).get_result()
+
+    try: 
+        result={
+            'headers': {'Content-Type':'application/json'}, 
+            'body': {'data':response} 
+            }
+        return result 
+
+    except:  
+        return { 
+            'statusCode': 404, 
+            'message': 'Something went wrong'
+            }
